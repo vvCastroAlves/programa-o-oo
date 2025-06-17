3a95458442b174ae8ceafed31d27f9b2905b4347
vvCastroAlves <viniciuscastroalves0409@gmail.com>
2025-06-17 19:51:48 -0300

    ED03

---
+++ FuncionarioCLT.java
@@ -0,0 +1,11 @@
+
+public class FuncionarioCLT extends Funcionario {
+    public FuncionarioCLT(String nome, double salarioBase) {
+        super(nome, salarioBase);
+    }
+
+    @Override
+    public double calcularSalario() {
+        return salarioBase + 300;
+    }
+}
